ef33f2f32cb2f30715a43d2a9cac3e500d1f0af3
Miravani sri <miravanisri123@gmail.com>
2023-06-12 20:04:53 +0530

    Todo-list

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+// import Toform from './components/Toform';
+import Tolist from './components/Tolist';
+
+
+
+function App() {
+  return (
+    <div className="todo-App">
+   <Tolist/>
+    </div>
+  );
+}
+
+export default App;
